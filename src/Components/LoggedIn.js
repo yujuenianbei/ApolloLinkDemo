a6e0bc5e7e4e9b8c1507cf26c511b2a4df687b5e
Martin Adams <heymartinadams@users.noreply.github.com>
2018-05-08 14:05:17 +0200

    Init.

--- src/Components/LoggedIn.js
+++ src/Components/LoggedIn.js
@@ -1,4 +1,4 @@
 // React
 import React from 'react'
 
-export default ({ user }) => (<center>Logged in!</center>)
+export default ({ user }) => (<center>Logged in!<br />However, if you now refresh the page, you’ll find that you’re no longer logged in! <span role='img' aria-label='surprised'>😳</span></center>)
